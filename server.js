57918596fb1695c7b06e794afdb963211e73c596
oladokun joshua <44656210+sukiejosh@users.noreply.github.com>
2020-06-22 23:41:07 +0100

    Update server.js (#22)

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const search = require('./src/api/search');
 const authorize = require('./src/api/authorize');
 const callback = require('./src/api/callback');
+const like = require('./src/api/like')
 const app = express();
 
 app.use(express.static('src/public'));
@@ -11,9 +12,18 @@ app.get('/', home);
 app.get('/oauth/authorize', authorize);
 app.get('/oauth/callback', callback);
 app.get('/api/tweets/:query?', getTweets);
+app.post('/api/timeline/mentions',likeUsers)
 
 function home(req, res) {
   res.sendFile(__dirname + '/src/views/index.html');
+}
+
+function likeUsers(req, res) {
+  //it likes users that tags it in hashtags(#) or mentions(@)
+  like(res)
+  // setInterval(() => {
+  //   like(res)
+  // }, 30000);
 }
 
 function getTweets(req, res) {
