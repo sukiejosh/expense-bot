d55cd68aef8a5aadc815dfb34a203bfd3d7f2902
piouson <piouson@gmail.com>
2020-06-23 11:54:10 +0100

    Fix heroku server crash
    - change `Twit = require('Twit')` to `Twit = require('twit')` in `src/api/mentions.js`
    - fix route conflicts, introduce `api/timelines/mentions` and `api/timelines/mentions/like`
    - update index.html
    - run lint:fix
    
    Resolve #26

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const search = require('./src/api/search');
 const authorize = require('./src/api/authorize');
 const callback = require('./src/api/callback');
-const like = require('./src/api/like')
+const like = require('./src/api/like');
 const getMentions = require('./src/api/mentions');
 const app = express();
 
@@ -13,19 +13,15 @@ app.get('/', home);
 app.get('/oauth/authorize', authorize);
 app.get('/oauth/callback', callback);
 app.get('/api/tweets/:query?', getTweets);
-app.get('/api/timeline/mentions', getMentions)
-app.post('/api/timeline/mentions',likeUsers)
+app.get('/api/timeline/mentions', getMentions);
+app.post('/api/timeline/mentions/like', likeUsers);
 
 function home(req, res) {
   res.sendFile(__dirname + '/src/views/index.html');
 }
 
 function likeUsers(req, res) {
-  //it likes users that tags it in hashtags(#) or mentions(@)
-  like(res)
-  // setInterval(() => {
-  //   like(res)
-  // }, 30000);
+  like(res);
 }
 
 function getTweets(req, res) {
